fc9b698db3a56d037623306c66fd9d472bcf45ed
Akos-1 <veronicagyasi@gmail.com>
2023-10-27 14:30:00 -0700

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,10 +2,5 @@
 # install a specific version of flask (2.1.0)
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
-}
-
-package { 'werkzeug':
-  ensure   => 'latest',
   provider => 'pip3',
 }
