3b198963eb7fdb0092887338d38b959b8027b019
Akos-1 <veronicagyasi@gmail.com>
2023-10-28 10:32:48 -0700

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,9 +2,13 @@
 # using Puppet to make changes to our configuration file
 
 file { '/etc/ssh/ssh_config':
-         ensure  => present,
-content =>"
-        #SSH client configuration
-        host"
-        IdentityFile /alx-system_engineering-devops/0x0B-ssh/school
-        PasswordAuthentication no	
+  ensure  => present,
+  content => "
+    #SSH client configuration
+    host
+        HostName 54.146.77.157
+        User ubuntu
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+   "
+}
