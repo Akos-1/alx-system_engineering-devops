405a348424d9520506c9d69c894b1fa5ef555663
Akos-1 <veronicagyasi@gmail.com>
2023-12-01 04:30:43 -0800

    updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""A bash script accept an integer as a
-parameter, which is the employee ID"""
+"""A bash script that accepts data from an api"""
+
 import requests
 import sys
 
@@ -17,42 +17,38 @@ def get_employee_todo_progress(employee_id):
     """
     # Replace this URL with the actual endpoint of your REST API
     url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
 
     # Make a GET request to the API
-    response = requests.get(url)
+    user_response = requests.get(user_url)
 
     # Check if the request was successful (status code 200)
+    if user_response.status_code != 200:
+        print("Error: Could not fetch employee details.")
+        return
+
+    # Extract employee name
+    user_data = user_response.json()
+    employee_name = user_data.get('name', f'Employee {employee_id}')
+
+    response = requests.get(url)
+
     if response.status_code == 200:
         todos = response.json()
 
-        # Extract employee name
-        employee_name = todos[0].get('username', '')
-
-        # Count the number of completed tasks
-        done_tasks = [task for task in todos if task.get('completed', False)]
-        num_done_tasks = len(done_tasks)
-
-        # Calculate the total number of tasks
         total_tasks = len(todos)
+        done_tasks = sum(1 for task in todos if task['done'])
 
-        # Determine status based on the number of completed tasks
-        status = "OK" if num_done_tasks > 0 else "Incorrect"
-
-        # Display the employee TODO list progress
-        progress_message = (
-            f'Employee Name: {status} '
-            f'({num_done_tasks}/{total_tasks}):'
-        )
-        print(progress_message)
-
-        # Display the titles of completed tasks
-        if done_tasks:
-            for task in done_tasks:
-                print(f'\t{task.get("title", "")}')
+        if total_tasks > 0:
+            progress_message = f"({done_tasks}/{total_tasks})"
         else:
-            print('\tNo completed tasks')
-    else:
-        print(f'Error: Unable to fetch TODO list for employee {employee_id}')
+            progress_message = "(No tasks)"
+
+        print(f"Employee {employee_name} is done with tasks{progress_message}: ")
+
+        for task in todos:
+            if task['done']:
+                print(f"\t {task['title']}")
 
 
 if __name__ == "__main__":
