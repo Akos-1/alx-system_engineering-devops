5e652d478aa25b9a08edee9b4ca1c1f2464f5083
Akos-1 <veronicagyasi@gmail.com>
2023-12-12 12:50:40 -0800

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-Recursive function to query Reddit API and for a subreddit.
+Recursively query Reddit API to get titles
+of all hot articles for a given subreddit.
 """
 
 import requests
-after = None
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=None, after=None):
     """
-    Recursively query Reddit API to get titles of all
-    hot articles for a given subreddit.
+    Recursively query Reddit API to get titles of
+    all hot articles for a given subreddit.
 
     Args:
         subreddit (str): The name of the subreddit.
@@ -21,35 +21,36 @@ def recurse(subreddit, hot_list=[]):
         list: List containing titles of hot articles,
         or None if no results are found.
     """
-    global after
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'MyRedditBot/1.0'}
-
     # Initialize hot_list if not provided
     if hot_list is None:
         hot_list = []
 
+    # Set User-Agent for API request
+    user_agent = {'User-Agent': 'MyRedditBot/1.0'}
+
     # Make the API request to get hot posts in the subreddit
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100'
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     params = {'after': after} if after else {}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    results = requests.get(
+        url,
+        params=params,
+        headers=user_agent,
+        allow_redirects=False
+    )
+    # Check if the request was successful (status code 200)
+    if results.status_code == 200:
+        # Extract data from the JSON response
+        data = results.json().get("data")
+        after_data = data.get("after")
+        # Recursively call for the next page
+        if after_data:
+            recurse(subreddit, hot_list, after=after_data)
 
-    # Check if the subreddit is not valid
-    if response.status_code == 404:
-        print(f"The subreddit '{subreddit}' does not exist.")
-        return None
+        # Extract titles and append to hot_list
+        all_titles = data.get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
 
-    # Parse the JSON response to get post data
-    data = response.json()
-
-    # Extract titles and append to hot_list
-    posts = data.get('data', {}).get('children', [])
-    for post in posts:
-        hot_list.append(post.get('data', {}).get('title'))
-
-    # Recursive call for the next page
-    after_token = data.get('data', {}).get('after')
-    if after_token:
-        recurse(subreddit, hot_list, after_token)
-
-    return hot_list if hot_list else None
+    # Return None if the subreddit is not valid
+    return None
